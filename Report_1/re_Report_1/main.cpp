589e9765fb8f39271ae3af79a4cc28031b173d7a
Kim Jaeeun <bak1916@gmail.com>
2022-11-10 09:35:34 +0900

    보충수업 과제 (최종 본)

---
+++ Report_1/re_Report_1/main.cpp
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+	char str[] = "C:/Document/Github/Test1/test.txt";
+	char* ptr = str;
+	char* ptrF[1000] = { NULL, };
+	char str2[] = "test2";
+
+	char* ptrsplit = strtok(str, "/");
+	int counter = 0;
+
+
+	while (ptrsplit != NULL)
+	{
+		ptrF[counter] = ptrsplit;
+		counter++;
+		ptrsplit = strtok(NULL, "/");
+	}
+	printf("\n\n========== 변경 전 ==========\n\n");
+	for (int i = 0; i < counter; i++)
+	{
+		printf("%s/", ptrF[i]);
+	}
+	printf("\n\n");
+	for (int i = 0; i < counter; i++)
+	{
+		printf("%d, %s\n", i, ptrF[i]);
+	}
+	printf("\n\n\========== 변경 후 ==========\n\n");
+	ptrF[3] = { str2 };
+
+
+	for (int i = 0; i < counter; i++)
+	{
+		printf("%s/", ptrF[i]);
+	}
+	printf("\n\n");
+	for (int i = 0; i < counter; i++)
+	{
+		printf("%d, %s\n", i, ptrF[i]);
+	}
+}
